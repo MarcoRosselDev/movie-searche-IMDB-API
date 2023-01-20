a113ba14d148e088fe17202b643f78f033273a8f
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-01-20 11:20:22 -0300

    stado de peliculas

--- src/App.js
+++ src/App.js
@@ -12,6 +12,22 @@ function App() {
   const [movie, setMovie] = useState({ title: "Loading Movies" });
   const [playing, setPlaying] = useState(false);
 
+  // funcion para realizar la peticion por get a la api
+
+  const fetchMovies = async (searchKey) => {
+    const type = searchKey ? "search" : "discover";
+    const { data: { results }
+  } = await axios.get(`${process.env.API_URL}/${type}/movie`, {
+      params: {
+        api_key: process.env.API_KEY,
+        query: searchKey.
+      },
+    })
+
+    setMovies(results)
+    setMovie(results[0])
+  };
+
   return (
     <div>
       <h1>hola</h1>
