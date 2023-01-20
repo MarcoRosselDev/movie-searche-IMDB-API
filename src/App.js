88eb64154385209f6804cda5b6765eced0c86296
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-01-20 09:29:38 -0300

    variables de estado por ahora

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import YouTube from "react-youtube";
 import "./App.css";
 
 function App() {
+  console.log(process.env);
+  // variables de estado
+  const [movies, setMovies] = useState([]);
+  const [searchKey, setSearchKey] = useState("");
+  const [trailer, setTrailer] = useState(null);
+  const [movie, setMovie] = useState({ title: "Loading Movies" });
+  const [playing, setPlaying] = useState(false);
+
   return (
     <div>
       <h1>hola</h1>
